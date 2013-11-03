c689ce2c7a4f976b0412b7827a096506b4d19ffc
root <root@raspberrypi>
2013-11-03 12:00:44 +0100

    Disabled LCD output

--- raspibrew.py
+++ raspibrew.py
@@ -234,17 +234,18 @@ def heatProcGPIO(cycle_time, duty_cycle, conn):
            
 # Main Temperature Control Process
 def tempControlProc(mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, boil_manage_temp, num_pnts_smooth, k_param, i_param, d_param, statusQ, conn):
-    
-        #initialize LCD
-        ser = serial.Serial("/dev/ttyAMA0", 9600)
-        ser.write("?BFF")
-        time.sleep(.1) #wait 100msec
-        ser.write("?f?a")
-        ser.write("?y0?x00PID off      ")
-        ser.write("?y1?x00HLT:")
-        ser.write("?y3?x00Heat: off      ")
-        ser.write("?D70609090600000000") #define degree symbol
-        time.sleep(.1) #wait 100msec
+   
+	if false: 
+        	#initialize LCD
+        	ser = serial.Serial("/dev/ttyAMA0", 9600)
+        	ser.write("?BFF")
+        	time.sleep(.1) #wait 100msec
+        	ser.write("?f?a")
+        	ser.write("?y0?x00PID off      ")
+        	ser.write("?y1?x00HLT:")
+        	ser.write("?y3?x00Heat: off      ")
+        	ser.write("?D70609090600000000") #define degree symbol
+        	time.sleep(.1) #wait 100msec
             
         p = current_process()
         print 'Starting:', p.name, p.pid
@@ -299,11 +300,12 @@ def tempControlProc(mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, bo
                 temp_C_str = "%3.2f" % temp_C
                 temp_F_str = "%3.2f" % temp_F
                 #write to LCD
-                ser.write("?y1?x05")
-                ser.write(temp_F_str)
-                ser.write("?7") #degree
-                time.sleep(.005) #wait 5msec
-                ser.write("F   ") 
+		if false:
+                	ser.write("?y1?x05")
+                	ser.write(temp_F_str)
+                	ser.write("?7") #degree
+                	time.sleep(.005) #wait 5msec
+                	ser.write("F   ") 
                 readytemp = True
                 
             if readytemp == True:        
@@ -336,9 +338,10 @@ def tempControlProc(mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, bo
             while parent_conn_heat.poll(): #Poll Heat Process Pipe
                 cycle_time, duty_cycle = parent_conn_heat.recv() #non blocking receive from Heat Process
                 #write to LCD
-                ser.write("?y2?x00Duty: ")
-                ser.write("%3.1f" % duty_cycle)
-                ser.write("%     ")    
+		if false:
+                	ser.write("?y2?x00Duty: ")
+                	ser.write("%3.1f" % duty_cycle)
+                	ser.write("%     ")    
                                  
             readyPOST = False
             while conn.poll(): #POST settings - Received POST from web browser or Android device
@@ -346,37 +349,41 @@ def tempControlProc(mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, bo
                 readyPOST = True
             if readyPOST == True:
                 if mode == "auto":
-                    ser.write("?y0?x00Auto Mode     ")
-                    ser.write("?y1?x00HLT:")
-                    ser.write("?y3?x00Set To: ")
-                    ser.write("%3.1f" % set_point)
-                    ser.write("?7") #degree
-                    time.sleep(.005) #wait 5msec
-                    ser.write("F   ") 
+		    if false:
+                    	ser.write("?y0?x00Auto Mode     ")
+                    	ser.write("?y1?x00HLT:")
+                    	ser.write("?y3?x00Set To: ")
+                    	ser.write("%3.1f" % set_point)
+                    	ser.write("?7") #degree
+                    	time.sleep(.005) #wait 5msec
+                    	ser.write("F   ") 
                     print "auto selected"
                     pid = PIDController.pidpy(cycle_time, k_param, i_param, d_param) #init pid
                     duty_cycle = pid.calcPID_reg4(temp_F_ma, set_point, True)
                     parent_conn_heat.send([cycle_time, duty_cycle])  
                 if mode == "boil":
-                    ser.write("?y0?x00Boil Mode     ")
-                    ser.write("?y1?x00BK: ")
-                    ser.write("?y3?x00Heat: on       ")
+		    if false:
+                    	ser.write("?y0?x00Boil Mode     ")
+                    	ser.write("?y1?x00BK: ")
+                    	ser.write("?y3?x00Heat: on       ")
                     print "boil selected"
                     boil_duty_cycle = duty_cycle_temp
                     duty_cycle = 100 #full power to boil manage temperature
                     manage_boil_trigger = True
                     parent_conn_heat.send([cycle_time, duty_cycle])  
                 if mode == "manual": 
-                    ser.write("?y0?x00Manual Mode     ")
-                    ser.write("?y1?x00BK: ")
-                    ser.write("?y3?x00Heat: on       ")
+		    if false:
+                    	ser.write("?y0?x00Manual Mode     ")
+                    	ser.write("?y1?x00BK: ")
+                    	ser.write("?y3?x00Heat: on       ")
                     print "manual selected"
                     duty_cycle = duty_cycle_temp
                     parent_conn_heat.send([cycle_time, duty_cycle])    
                 if mode == "off":
-                    ser.write("?y0?x00PID off      ")
-                    ser.write("?y1?x00HLT:")
-                    ser.write("?y3?x00Heat: off      ")
+		    if false:
+                    	ser.write("?y0?x00PID off      ")
+                    	ser.write("?y1?x00HLT:")
+                    	ser.write("?y3?x00Heat: off      ")
                     print "off selected"
                     duty_cycle = 0
                     parent_conn_heat.send([cycle_time, duty_cycle])
