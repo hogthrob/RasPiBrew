df5f8b0a7691e1b22638fe7b69811fe8a1ad2c73
hogthrob <hogthrob@users.noreply.github.com>
2013-11-07 22:38:27 +0100

    Added option to automatically issue user confirmation for testing

--- call_test.py
+++ call_test.py
@@ -1,9 +1,20 @@
 import json
 import urllib
 import time
+import raspibrew
+from datetime import datetime,date,timedelta
+from datetime import time as dtime
 
-speedUp = 32.0
-updateInterval = 2.0
+
+if raspibrew.runAsSimulation == 0:
+	speedUp = 1.0
+	updateInterval = 1.0
+	autoConfirm = 0
+else:	
+	speedUp = raspibrew.speedUp 
+	updateInterval = 2.0
+	autoConfirm = 1
+
 hoptime = -1.0
 
 def enum(**enums):
@@ -23,15 +34,20 @@ def fetch_thing(url, params, method):
 
 def control(num,mode,setpoint,dutycycle,cycletime):
         content, response_code = fetch_thing(
-                              'http://localhost:8080/',
+                              'http://localhost:'+ str(8079+num)+ '/',
                               {'mode': mode, 'setpoint': setpoint, 'k': 30, 'i': 810, 'd': 45, 'dutycycle': dutycycle, 'cycletime': cycletime},
                               'POST'
                          )
 
 def Init():
         content, response_code = fetch_thing(
-                              'http://localhost:8080/',
+                              'http://localhost:'+ str(8080)+ '/',
                               {'mode': 'off', 'setpoint': 0, 'k': 30, 'i': 810, 'd': 45, 'dutycycle': 0, 'cycletime': 5.0},
+                              'POST'
+                         )
+        content, response_code = fetch_thing(
+                              'http://localhost:'+ str(8081)+ '/',
+                              {'mode': 'off', 'setpoint': 0, 'k': 30, 'i': 810, 'd': 45, 'dutycycle': 0, 'cycletime': 600.0},
                               'POST'
                          )
 	print "Ready!"		 
@@ -39,7 +55,7 @@ def Init():
 
 def status(num):
         content, response_code = fetch_thing(
-                              'http://localhost:8080/getstatus',
+                              'http://localhost:'+ str(8079+num)+ '/getstatus',
                               {'num': 1},
                               'GET'
                          )
@@ -80,8 +96,10 @@ def WaitForTime(waittime,message):
 		tempTime = tempTime - updateInterval 
 
 def WaitForUserConfirm(message):
-	print message
-	print "Press OK to Confirm"
+	print message 
+	if (autoConfirm == 0):
+		print '\a\a\a'
+		nb = raw_input('Press Enter to Confirm')
 
 def StopHeat():
 	print "Stopping heater"
@@ -91,12 +109,12 @@ def StopHeat():
 def StopPump():
 	print "Stopping Pump"
 	data = status(1)
-	# control(2,'off',0,0,data['cycle_time'])
+	control(2,'off',0,0,data['cycle_time'])
 
 def ActivatePump():
 	print "Starting Pump in Continous Mode"
 	data = status(1)
-	# control(2,'manual',0,100,data['cycle_time'])
+	control(2,'manual',0,100,data['cycle_time'])
 
 def StartHopTimer(hoptime):
 	global hopTime
@@ -114,16 +132,25 @@ def WaitForHopTimerDone():
 	while time.time() < hopTime:
 		time.sleep(updateInterval/speedUp)
 
+def WaitUntilTime(waitdays,hour,min,message):
+	whenStart = datetime.combine(datetime.today() + timedelta(days = waitdays), dtime(hour,min))
+	print message + " @ " + whenStart.isoformat(' ')
+	while datetime.now() < whenStart:
+		time.sleep(updateInterval)
+
 def ActivatePumpInterval(duty,intervaltime):
 	print "Starting Pump in Interval Mode on=",float(intervaltime)*duty/100.0,"min, off=",(100.0-float(duty))/100.0*intervaltime,"min"
-	# control(2,'manual',0,duty,intervaltime*60)
+	control(2,'manual',0,duty,intervaltime*60)
 
 
 Init()
 WaitForUserConfirm('Filled Water?')
 ActivatePump()
 WaitForTime(1,"Pump Init")
-## WaitUntilTime(WhenStart,"Waiting for starting time")
+StopPump()
+WaitForTime(1,"Pump Init")
+WaitUntilTime(0,21,06,"Waiting for starting time")
+ActivatePump()
 WaitForHeat(70,'Waiting for Mash In Temp')
 StopPump()
 WaitForUserConfirm('Mashed in?')
