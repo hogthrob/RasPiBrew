c1bf6e1833f1bc6b472a843d91da0599523a8357
hogthrob <hogthrob@users.noreply.github.com>
2013-11-16 07:29:21 +0100

    Small change after fixing performance issue

--- raspibrew.py
+++ raspibrew.py
@@ -385,7 +385,6 @@ def heatProcGeneric(style,configFile, num,cycle_time, duty_cycle, conn):
     while (True):
         pwm.cycle_time, pwm.duty_cycle = conn.recv()
         conn.send([pwm.cycle_time, pwm.duty_cycle])
-        time.sleep(1.0/speedUp)
 
 # Stand Alone Heat Process using I2C
 def heatProcI2C(configFile, num, cycle_time, duty_cycle, conn):
